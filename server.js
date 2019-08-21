2e2f2767393476211a20cc49bdf57e3ebd083734
OKali <o.kalinowski27@gmail.com>
2019-08-21 10:05:06 -0400

    removed unneccessary comments

--- server.js
+++ server.js
@@ -3,41 +3,28 @@ var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
 var PORT = 8080;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
 app.set("views", "./views")
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/ScrapingNews", { useNewUrlParser: true });
 
 // Routes
-
 app.get("/", function(req, res) {
-    // res.send("Didn't want a blank page")
   db.Article.find({})
   .then(data => {
 
@@ -48,19 +35,13 @@ app.get("/", function(req, res) {
   }).catch(err => res.send(err));
 });
 
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
   axios.get("https://www.espn.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
     $(".contentItem__content").each(function(i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .find(".contentItem__title")
         .text();
@@ -71,22 +52,15 @@ app.get("/scrape", function(req, res) {
         .children("a")
         .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
       if (result.title && result.summary && result.link) {
         db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
-        //   if (title && summary && link) {
-
-        //   }
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
-      }
-     
+      } 
     });
 
     // Send a message to the client
@@ -111,38 +85,28 @@ app.get("/scrape", function(req, res) {
 
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     //   return db.Note.find({})
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   db.Note.create(req.body)
     .then(function(dbNote) {
@@ -171,7 +135,6 @@ app.get("/notes/", function(req, res) {
     })
 })
 
-// Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
