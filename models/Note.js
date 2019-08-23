211f8e744a4619c27d9ae1f6ee17afe63106e414
OKali <o.kalinowski27@gmail.com>
2019-08-23 10:17:10 -0400

    connection through note id in article collection

--- models/Note.js
+++ models/Note.js
@@ -4,11 +4,11 @@ var Schema = mongoose.Schema;
 
 var NoteSchema = new Schema({
   title: String,
-  body: String,
-  _articleId: {
-    type: Schema.Types.ObjectId, 
-    ref: "Article"
-  }
+  body: String
+  // _articleId: {
+  //   type: Schema.Types.ObjectId, 
+  //   ref: "Article"
+  // }
 });
 
 var Note = mongoose.model("Note", NoteSchema);
